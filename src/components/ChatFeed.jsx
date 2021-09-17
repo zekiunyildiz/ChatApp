1862b392cbc6cee3b49d717dfa2e1c239e76e9a0
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-17 03:26:56 +0300

    ChatFeed w

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,35 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
 const ChatFeed = (props) => {
-    console.log(props);
+    const {chats, activeChat, userName, messages} = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index -1];
+            const isMyMessage = userName === message.sender.username;
+
+            return(
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage/>
+                            : <TheirMessage/>
+                        }
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessages()
 
     return(
         <div>
