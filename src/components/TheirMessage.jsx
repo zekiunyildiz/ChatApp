87d62cb370fd55fd5ff6612fc5a1ae58e52c79fa
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-27 03:01:46 +0300

    TheirMessage w

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,8 +1,17 @@
-const TheirMessage = () => {
+const TheirMessage = ({lastMessage, message}) => {
+
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
 
     return(
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{backgroundImage:`url(${message?.sender?.avatar})`}}
+                />
+            )}
+            
         </div>
     )
 
